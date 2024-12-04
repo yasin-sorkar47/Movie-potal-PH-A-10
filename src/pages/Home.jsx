8a5281756b38c0fb820fa4e963b69afaf7f4d240
yasin-sorkar47 <mdyacinsorkar17@gmail.com>
2024-12-04 12:04:21 +0600

    copleated the featured movies desing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../components/Banner";
+import FeaturedMovies from "../components/FeaturedMovies";
 
 export default function Home() {
   return (
     <>
       <Banner />
+      <FeaturedMovies />
     </>
   );
 }
